cc5e2e41a9ddd38a1d04c1dfcb68ae86dcfc111f
DannySklyarevskiy <137563220+DannySklyarevskiy@users.noreply.github.com>
2023-10-21 00:13:43 -0700

    Added mysql database functionality, message info is now saved to database

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 # IMPORT DISCORD.PY. ALLOWS ACCESS TO DISCORD'S API.
+# 
 import discord
+import mysql.connector
 
 # GETS THE CLIENT OBJECT FROM DISCORD.PY. CLIENT IS SYNONYMOUS WITH BOT.
 intents = discord.Intents.default()
 intents.message_content = True
 bot = discord.Client(intents=intents)
 
+# GETS THE FILE WITH ALL PRIVATE INFORMATION
+secret = open("secret.txt").readlines()
+
+connection = mysql.connector.connect(host=secret[1],
+                                        database=secret[2],
+                                        user=secret[3],
+                                        password=secret[4])
+print(connection)
 # EVENT LISTENER FOR WHEN THE BOT HAS SWITCHED FROM OFFLINE TO ONLINE.
 @bot.event
 async def on_ready():
@@ -34,13 +44,27 @@ async def on_message(message):
 
 		# SET OF VARIABLES NEEDED TO SHOW RESPONSE
 		content = message.content
+		contentHex = content.encode().hex()
 		time = str(msg.created_at)[:10]
 		user = msg.author.id
 
-		# PRINTS OUT INFORMATION ABOUT MESSAGE
+		# PRINTS OUT INFORMATION ABOUT THE MESSAGE
 		await channel.send(str((await bot.fetch_user(user)).display_name) + 
 					 " said \"" + content + 
 					 "\" at " + time)
+		
+		# CREATES A CURSOR THAT INTERACTS WITH THE MYSQL DATABASE
+		cursor = connection.cursor()
+		
+		# CREATES A UNIQUE ID FOR NEW MESSAGE
+		cursor.execute("SELECT MAX(id) FROM messages")
+		id = cursor.fetchone()[0]+1
+
+		# ADDS THE INFORMATION ABOUT THE MESSAGE TO THE MYSQL DATABASE
+		sql = "INSERT INTO messages (id, message_content, user_id, date_sent) VALUES (%s, %s, %s, %s)"
+		val = (id, content, user, time)
+		cursor.execute(sql, val)
+		connection.commit()
 
 # EXECUTES THE BOT WITH THE SPECIFIED TOKEN. TOKEN IS HIDDEN IN FILE FOR PRIVACY.
-bot.run(open("token.txt", "r").read())
+bot.run(secret[0])
